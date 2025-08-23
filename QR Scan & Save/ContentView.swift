fddde1dfb649509337d3b34d762c93278a91bfaf
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-08-23 22:08:26 +0700

    Initial Commit

---
+++ QR Scan & Save/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QR Scan & Save
+//
+//  Created by Nguyen Quang Minh on 8/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
